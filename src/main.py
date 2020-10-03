7f429cbb8f0ddd5a511a0913440445c15df7d340
spelley <stevenpelley@gmail.com>
2020-10-03 15:58:41 -0400

    add logging with a basic config TODO: file rotation.  Unlikely to be a problem for a while, or ever? consider some sort of json logging.  This only matters if we ever need to ingest logs into a database.

--- src/main.py
+++ src/main.py
@@ -1,16 +1,36 @@
 """Execute main, test logic.  To be replaced."""
 
+import logging
+
+
+# each module/file should provide a global-level logger using this statement
+logger = logging.getLogger(__name__)
+
 
 def main() -> None:
     """Run the main function and logic."""
+    setup_logging()
+    logger.info('main starting: %s', 'asdf')
+    logger.warning('sample warn')
     s = "eric writes silly variable names"
     test_string = "Hello"
-    print("{} - {}".format(s, test_string))
+    logger.info('%s - %s', s, test_string)
 
 
 def test_func_with_type(arg: str) -> bool:
     """Return an arbitrary comparison."""
     return arg == "nothing"
+
+
+def setup_logging() -> None:
+    """Set up logging."""
+    # TODO:
+    # add file rotation
+    # make json logging so we can easily ingest elsewhere
+    logging.basicConfig(
+        filename='gloomhaven-monster-ai.log',
+        level=logging.DEBUG,
+        format='%(asctime)s - %(name)s - %(threadName)s  - %(message)s')
 
 
 if __name__ == '__main__':
